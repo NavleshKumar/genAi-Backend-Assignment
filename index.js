f09945b2aa5742d008a8e4e443ea0885a58697c0
Navlesh Kumar <78998392+NavleshKumar@users.noreply.github.com>
2023-12-19 15:40:00 +0530

    Update index.js
    updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!!')
 })
   
 app.post('/register', (req, res) => {
